787c31447ef34601e88dd69ae2f5263a1f8d1877
wertezaj <99995054+wertezaj@users.noreply.github.com>
2026-05-31 17:47:11 +0200

    deploy_v4

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD AWS project"
+    return "CI/CD AWS project v2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
