dcb670ba125df502a5af169930af659121e301c9
wertezaj <99995054+wertezaj@users.noreply.github.com>
2026-05-31 19:01:12 +0200

    extended pipeline

---
+++ test_main.py
@@ -0,0 +1,13 @@
+import pytest
+from main import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_endpoint(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"CI/CD AWS project" in response.data
